d8f7c43e6740adee8fee91e87c36d5c8486c770f
Fangjun Zhou <fangjun.zhou02@gmail.com>
2023-08-27 16:43:31 -0500

    Added MKL BLAS benchmark

---
+++ benchmarks/mkl/blas_level3_benchmark.cpp
@@ -0,0 +1,29 @@
+#include <benchmark/benchmark.h>
+#include <cstddef>
+#include <cstdlib>
+#include <mkl.h>
+#include <mkl_cblas.h>
+#include <vector>
+
+static void mkl_gemm_benchmark(benchmark::State &state) {
+  size_t matSize = state.range(0);
+  // Init random input matricies.
+  std::vector<float> matA(matSize * matSize);
+  std::vector<float> matB(matSize * matSize);
+  for (size_t i = 0; i < matSize; i++) {
+    for (size_t j = 0; j < matSize; j++) {
+      size_t idx = i * matSize + j;
+      matA[idx] = std::rand();
+      matB[idx] = std::rand();
+    }
+  }
+  // Output matrix.
+  std::vector<float> matC(matSize * matSize);
+  for (auto _ : state) {
+    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, matSize, matSize,
+                matSize, 1, matA.data(), matSize, matB.data(), matSize, 0,
+                matC.data(), matSize);
+  }
+}
+
+BENCHMARK(mkl_gemm_benchmark)->RangeMultiplier(2)->Range(2, 1024);
