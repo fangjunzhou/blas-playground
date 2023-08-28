bc2bd0678ea39275ea933759088fb57ea25a1be1
Fangjun Zhou <fangjun.zhou02@gmail.com>
2023-08-27 23:09:03 -0500

    Added benchmark for vanilla GEMM

--- benchmarks/blas_level3_benchmarks.cpp
+++ benchmarks/blas_level3_benchmarks.cpp
@@ -5,6 +5,8 @@
 #include <mkl_cblas.h>
 #include <vector>
 
+#include "gemm/gemm.h"
+
 static void mkl_gemm_benchmark(benchmark::State &state) {
   size_t matSize = state.range(0);
   // Init random input matricies.
@@ -27,3 +29,24 @@ static void mkl_gemm_benchmark(benchmark::State &state) {
 }
 
 BENCHMARK(mkl_gemm_benchmark)->RangeMultiplier(2)->Range(2, 1024);
+
+static void vanilla_gemm_benchmark(benchmark::State &state) {
+  size_t matSize = state.range(0);
+  // Init random input matricies.
+  std::vector<float> matA(matSize * matSize);
+  std::vector<float> matB(matSize * matSize);
+  for (size_t i = 0; i < matSize; i++) {
+    for (size_t j = 0; j < matSize; j++) {
+      size_t idx = i * matSize + j;
+      matA[idx] = (float)std::rand() / (float)RAND_MAX;
+      matB[idx] = (float)std::rand() / (float)RAND_MAX;
+    }
+  }
+  // Output matrix.
+  std::vector<float> matC(matSize * matSize);
+  for (auto _ : state) {
+    gemm_vanilla(matA, matB, matC, matSize);
+  }
+}
+
+BENCHMARK(vanilla_gemm_benchmark)->RangeMultiplier(2)->Range(2, 1024);
