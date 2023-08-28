2f06cfad3b526d0cb556ccecbf8f31b14fd018cf
Fangjun Zhou <fangjun.zhou02@gmail.com>
2023-08-28 00:05:35 -0500

    Added tests for gemmVanillaParallel

--- tests/blas_level3_tests.cpp
+++ tests/blas_level3_tests.cpp
@@ -7,36 +7,55 @@
 
 #include "gemm/gemm.h"
 
-TEST(blas_leve3_test, gemm_vanilla) {
-  // The size of the matrix.
-  const size_t matSize = 16;
-  // Allowed error size.
-  const float allowedErr = 1e-5;
+#define MAT_SIZE 16
+#define ALLOWED_ERR 1e-5
 
-  // Init random input matricies.
-  std::vector<float> matA(matSize * matSize);
-  std::vector<float> matB(matSize * matSize);
-  for (size_t i = 0; i < matSize; i++) {
-    for (size_t j = 0; j < matSize; j++) {
-      size_t idx = i * matSize + j;
-      matA[idx] = (float)std::rand() / (float)RAND_MAX;
-      matB[idx] = (float)std::rand() / (float)RAND_MAX;
+class BlasLevel3Test : public ::testing::Test {
+protected:
+  std::vector<float> matA;
+  std::vector<float> matB;
+  std::vector<float> matC;
+  std::vector<float> matCRef;
+
+  void SetUp() override {
+    this->matA.resize(MAT_SIZE * MAT_SIZE);
+    this->matB.resize(MAT_SIZE * MAT_SIZE);
+    for (size_t i = 0; i < MAT_SIZE; i++) {
+      for (size_t j = 0; j < MAT_SIZE; j++) {
+        size_t idx = i * MAT_SIZE + j;
+        matA[idx] = (float)std::rand() / (float)RAND_MAX;
+        matB[idx] = (float)std::rand() / (float)RAND_MAX;
+      }
     }
+    this->matC.resize(MAT_SIZE * MAT_SIZE);
+    this->matCRef.resize(MAT_SIZE * MAT_SIZE);
   }
-  // Output matrix.
-  std::vector<float> matC(matSize * matSize);
-  // MKL reference matrix.
-  std::vector<float> matCRef(matSize * matSize);
+};
 
+TEST_F(BlasLevel3Test, gemmVanilla) {
   // MKL reference
-  cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, matSize, matSize,
-              matSize, 1, matA.data(), matSize, matB.data(), matSize, 0,
-              matCRef.data(), matSize);
+  cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, MAT_SIZE, MAT_SIZE,
+              MAT_SIZE, 1, this->matA.data(), MAT_SIZE, this->matB.data(),
+              MAT_SIZE, 0, this->matCRef.data(), MAT_SIZE);
   // Vanilla GEMM implementation.
-  gemmVanilla(matA, matB, matC, matSize);
+  gemmVanilla(this->matA, this->matB, this->matC, MAT_SIZE);
 
   // Compare the result.
-  for (size_t i = 0; i < matSize * matSize; i++) {
-    EXPECT_NEAR(matC[i], matCRef[i], allowedErr);
+  for (size_t i = 0; i < MAT_SIZE * MAT_SIZE; i++) {
+    EXPECT_NEAR(matC[i], matCRef[i], ALLOWED_ERR);
+  }
+}
+
+TEST_F(BlasLevel3Test, gemmVanillaParallel) {
+  // MKL reference
+  cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, MAT_SIZE, MAT_SIZE,
+              MAT_SIZE, 1, this->matA.data(), MAT_SIZE, this->matB.data(),
+              MAT_SIZE, 0, this->matCRef.data(), MAT_SIZE);
+  // Vanilla GEMM implementation.
+  gemmVanillaParallel(this->matA, this->matB, this->matC, MAT_SIZE);
+
+  // Compare the result.
+  for (size_t i = 0; i < MAT_SIZE * MAT_SIZE; i++) {
+    EXPECT_NEAR(matC[i], matCRef[i], ALLOWED_ERR);
   }
 }
